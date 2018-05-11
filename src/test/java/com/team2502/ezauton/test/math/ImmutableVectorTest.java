72b690e96ca74177becd53e484241abd95b802a8
Andrew Gazelka <andrew.gazelka@gmail.com>
2018-05-11 13:02:23 -0500

    add more unit tests

--- src/test/java/com/team2502/ezauton/test/math/ImmutableVectorTest.java
+++ src/test/java/com/team2502/ezauton/test/math/ImmutableVectorTest.java
@@ -1,15 +1,54 @@
 package com.team2502.ezauton.test.math;
 
 import com.team2502.ezauton.trajectory.geometry.ImmutableVector;
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ImmutableVectorTest {
 
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWrongSize()
+    {
+        new ImmutableVector(1,1).assertSize(1);
+    }
     @Test
     public void testEq()
     {
-        Assert.assertEquals(new ImmutableVector(1,1),new ImmutableVector(1,1));
-        Assert.assertNotEquals(new ImmutableVector(1,1),new ImmutableVector(2,1));
+        assertEquals(new ImmutableVector(1,1),new ImmutableVector(1,1));
+        assertNotEquals(new ImmutableVector(1,1),new ImmutableVector(2,1));
+    }
+
+    @Test
+    public void testOf()
+    {
+        assertEquals(new ImmutableVector(4,4,4),ImmutableVector.of(4,3));
+    }
+
+    @Test
+    public void testMultiply()
+    {
+        assertEquals(new ImmutableVector(3,6,9),new ImmutableVector(1,2,3).mul(3));
+    }
+
+    @Test
+    public void testDot()
+    {
+        assertEquals(0,new ImmutableVector(1,0,0).dot(new ImmutableVector(0,1,0)),1E-6);
+    }
+
+    @Test
+    public void testMag()
+    {
+        assertEquals(Math.sqrt(27),ImmutableVector.of(3,3).mag(),1E-6);
+    }
+
+    @Test
+    public void testHashCode()
+    {
+        assertTrue(ImmutableVector.of(3,4).hashCode() == ImmutableVector.of(3,4).hashCode());
     }
 }
