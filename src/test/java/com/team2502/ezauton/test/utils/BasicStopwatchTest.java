9c31e293451405a661f30f4153e4ce8e8a8007a3
Andrew Gazelka <andrew.gazelka@gmail.com>
2018-05-11 20:54:34 -0500

    faster unit tests

--- src/test/java/com/team2502/ezauton/test/utils/BasicStopwatchTest.java
+++ src/test/java/com/team2502/ezauton/test/utils/BasicStopwatchTest.java
@@ -11,37 +11,56 @@ public class BasicStopwatchTest
     private static double DELTA = 1E-5;
 
     @Test
-    public void testRead() throws InterruptedException
+    public void testRead()
     {
-        BasicStopwatch stopwatch = new BasicStopwatch();
-        Thread.sleep(1000);
-        assertEquals(1000, stopwatch.read(), 7); // Delta of 5 ms
-        Thread.sleep(1000);
-        assertEquals(2000, stopwatch.read(), 14); // Delta of 10 ms
+        new Thread (() -> {
+            BasicStopwatch stopwatch = new BasicStopwatch();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            assertEquals(1000, stopwatch.read(), 7); // Delta of 5 ms
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            assertEquals(2000, stopwatch.read(), 14); // Delta of 10 ms
+        }).start();
     }
 
     @Test
-    public void testPop() throws InterruptedException
-    {
-        BasicStopwatch stopwatch = new BasicStopwatch();
-        Thread.sleep(1000);
-        assertEquals(1000, stopwatch.pop(), 5); // Delta of 10 ms
-        assertEquals(0, stopwatch.read(), 1); // Delta of 1 ms
+    public void testPop() {
+        new Thread (() -> {
+            BasicStopwatch stopwatch = new BasicStopwatch();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            assertEquals(1000, stopwatch.pop(), 5); // Delta of 10 ms
+            assertEquals(0, stopwatch.read(), 1); // Delta of 1 ms
+        }).start();
     }
 
     @Test
-    public void testReset() throws InterruptedException
-    {
-        BasicStopwatch stopwatch = new BasicStopwatch();
-        Thread.sleep(1000);
-        assertEquals(1000, stopwatch.read(), 5); // Delta of 10 ms
-        stopwatch.reset();
-        assertEquals(0, stopwatch.read(), 1); // Delta of 1 ms
+    public void testReset() {
+        new Thread (() -> {
+            BasicStopwatch stopwatch = new BasicStopwatch();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            assertEquals(1000, stopwatch.read(), 5); // Delta of 10 ms
+            stopwatch.reset();
+            assertEquals(0, stopwatch.read(), 1); // Delta of 1 ms
+        });
     }
 
     @Test
-    public void testIsInit() throws InterruptedException
-    {
+    public void testIsInit() {
         BasicStopwatch stopwatch = new BasicStopwatch();
         assertTrue(stopwatch.isInit());
     }
