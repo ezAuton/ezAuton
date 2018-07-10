8954b28c4bfa4368b0aff361b20207037231edc7
Andrew Gazelka <andrew.gazelka@gmail.com>
2018-07-10 15:26:31 -0500

    add more actuator unit tests

--- src/main/java/com/team2502/ezauton/actuators/implementations/BoundedVelocityProcessor.java
+++ src/main/java/com/team2502/ezauton/actuators/implementations/BoundedVelocityProcessor.java
@@ -6,24 +6,28 @@ import com.team2502.ezauton.actuators.VelocityProcessor;
 public class BoundedVelocityProcessor extends VelocityProcessor
 {
 
-    private final double maxVel;
+    private final double maxSpeed;
 
-    public BoundedVelocityProcessor(IVelocityMotor toApply, double maxVel)
+    public BoundedVelocityProcessor(IVelocityMotor toApply, double maxSpeed)
     {
         super(toApply);
-        this.maxVel = maxVel;
+        if(maxSpeed <= 0)
+        {
+            throw new IllegalArgumentException("maxSpeed must be a positive number!");
+        }
+        this.maxSpeed = maxSpeed;
     }
 
     @Override
     public void runVelocity(double targetVelocity)
     {
-        if(targetVelocity > maxVel)
+        if(targetVelocity > maxSpeed)
         {
-            getToApply().runVelocity(maxVel);
+            getToApply().runVelocity(maxSpeed);
         }
-        else if(targetVelocity < -maxVel)
+        else if(targetVelocity < -maxSpeed)
         {
-            getToApply().runVelocity(-maxVel);
+            getToApply().runVelocity(-maxSpeed);
         }
         else
         {
--- src/test/java/com/team2502/ezauton/test/utils/ActuatorsTest.java
+++ src/test/java/com/team2502/ezauton/test/utils/ActuatorsTest.java
@@ -4,6 +4,9 @@ import com.google.common.util.concurrent.AtomicDouble;
 import com.team2502.ezauton.actuators.Actuators;
 import com.team2502.ezauton.actuators.IVelocityMotor;
 import com.team2502.ezauton.actuators.IVoltageMotor;
+import com.team2502.ezauton.actuators.implementations.BoundedVelocityProcessor;
+import com.team2502.ezauton.actuators.implementations.RampUpVelocityProcessor;
+import com.team2502.ezauton.utils.SimulatedStopwatch;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -37,5 +40,71 @@ public class ActuatorsTest
 
         velocityMotor.runVelocity(-20);
         assertEquals(-1D, atomicDouble.doubleValue(), 1E-6);
+    }
+
+    @Test
+    public void testRampUpVelocityProcessor()
+    {
+        AtomicDouble velocity = new AtomicDouble();
+        IVelocityMotor velocityMotor = velocity::set;
+
+        SimulatedStopwatch stopwatch = new SimulatedStopwatch(1);
+
+        RampUpVelocityProcessor velocityProcessor = new RampUpVelocityProcessor(velocityMotor, stopwatch, 1);
+
+        velocityProcessor.runVelocity(2);
+        stopwatch.progress();
+        velocityProcessor.update();
+
+        assertEquals(1, velocity.doubleValue(), 1E-6);
+
+        stopwatch.progress();
+        velocityProcessor.update();
+
+        assertEquals(2, velocity.doubleValue(), 1E-6);
+
+        stopwatch.progress();
+        velocityProcessor.update();
+
+        assertEquals(2, velocity.doubleValue(), 1E-6);
+
+        velocityProcessor.runVelocity(1);
+        stopwatch.progress(0.99D);
+        velocityProcessor.update();
+        assertEquals(1.01D, velocity.doubleValue(), 1E-6);
+
+        velocityProcessor.runVelocity(10);
+        stopwatch.progress(0.99D);
+        velocityProcessor.update();
+        assertEquals(2D, velocity.doubleValue(), 1E-6);
+        assertEquals(2D, velocityProcessor.getLastVelocity(), 1E-6);
+    }
+
+    @Test
+    public void testBoundedVelocityProcessor()
+    {
+        AtomicDouble velocity = new AtomicDouble();
+        IVelocityMotor velocityMotor = velocity::set;
+
+        BoundedVelocityProcessor velocityProcessor = new BoundedVelocityProcessor(velocityMotor, 16);
+
+        velocityProcessor.runVelocity(1);
+        assertEquals(1, velocity.doubleValue(), 1E-6);
+
+        velocityProcessor.runVelocity(-15);
+        assertEquals(-15, velocity.doubleValue(), 1E-6);
+
+        velocityProcessor.runVelocity(-16);
+        assertEquals(-16, velocity.doubleValue(), 1E-6);
+
+        velocityProcessor.runVelocity(16);
+        assertEquals(16, velocity.doubleValue(), 1E-6);
+
+        velocityProcessor.runVelocity(-17);
+        assertEquals(-16, velocity.doubleValue(), 1E-6);
+
+        velocityProcessor.runVelocity(18);
+        assertEquals(16, velocity.doubleValue(), 1E-6);
+
     }
 }
