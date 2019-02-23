91b09b59bd58b2d8ff5137fde3a1856b6390fe6a
Andrew Gazelka <andrew.gazelka@gmail.com>
2019-02-23 14:01:55 -0600

    make CommandCreator use new run system API

--- WPILib/src/main/java/com/github/ezauton/wpilib/command/CommandCreator.java
+++ WPILib/src/main/java/com/github/ezauton/wpilib/command/CommandCreator.java
@@ -1,7 +1,10 @@
 package com.github.ezauton.wpilib.command;
 
 import com.github.ezauton.core.action.IAction;
+import com.github.ezauton.core.simulation.ActionScheduler;
 import edu.wpi.first.wpilibj.command.Command;
+
+import java.util.concurrent.Future;
 
 /**
  * Creates a action from an {@link IAction}. This allows for abstraction and use of commands in simulators.
@@ -9,22 +12,22 @@ import edu.wpi.first.wpilibj.command.Command;
 public class CommandCreator extends Command {
 
     private final IAction action;
-    private Thread thread;
-    private boolean finished = false;
+    private final ActionScheduler actionScheduler;
+    private Future<Void> voidFuture = null;
 
     /**
      * Create a action from a PeriodicAction
      *
      * @param action The action to run as a action
      */
-    public CommandCreator(IAction action) {
+    public CommandCreator(IAction action, ActionScheduler actionScheduler) {
         this.action = action;
+        this.actionScheduler = actionScheduler;
     }
 
     @Override
     protected void initialize() {
-        thread = new ProcessBuilder(action).start();
-        action.onFinish(() -> finished = true);
+        voidFuture = actionScheduler.scheduleAction(action);
     }
 
     @Override
@@ -34,18 +37,18 @@ public class CommandCreator extends Command {
 
     @Override
     protected boolean isFinished() {
-        return finished;
-    }
-
-    @Override
-    protected void end() {
-        action.end();
-        thread.stop();
+        if(voidFuture == null) throw new IllegalStateException("somehow command has not been initialized before running");
+        return voidFuture.isDone();
     }
 
     @Override
     protected void interrupted() {
-        action.end();
-        thread.interrupt();
+        if(voidFuture == null) throw new IllegalStateException("somehow command has not been initialized before running");
+        try {
+            action.interrupted();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        voidFuture.cancel(true);
     }
 }
