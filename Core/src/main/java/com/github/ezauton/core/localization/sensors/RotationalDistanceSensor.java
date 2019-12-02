d061514f6c61bff357501747193d5d92cd16c851
Ritik Mishra <ritik.mishra314@gmail.com>
2019-12-02 13:23:26 -0600

    Add onto javadoc for RotationalDistSensor

--- Core/src/main/java/com/github/ezauton/core/localization/sensors/RotationalDistanceSensor.java
+++ Core/src/main/java/com/github/ezauton/core/localization/sensors/RotationalDistanceSensor.java
@@ -2,6 +2,10 @@ package com.github.ezauton.core.localization.sensors;
 
 /**
  * A sensor which can record revolutions/s and revolutions as a distance
+ *
+ * Knows all about rotations and rotational velocity
+ *
+ * Implicitly accounts for encoder ticks
  */
 public interface RotationalDistanceSensor extends Tachometer {
     /**
