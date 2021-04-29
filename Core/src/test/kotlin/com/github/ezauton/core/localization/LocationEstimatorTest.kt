6568267cec736e68088769fde692c2b0c7b7d632
Andrew Gazelka <andrew.gazelka@gmail.com>
2021-04-29 16:42:52 -0500

    add location estimator test

--- Core/src/test/kotlin/com/github/ezauton/core/localization/LocationEstimatorTest.kt
+++ Core/src/test/kotlin/com/github/ezauton/core/localization/LocationEstimatorTest.kt
@@ -1,29 +1,30 @@
-//package com.github.ezauton.core.localization
-//
-//import com.github.ezauton.conversion.ScalarVector
-//import org.junit.jupiter.api.Assertions.assertEquals
-//import org.junit.jupiter.api.Test
-//
-//class ocationEstimatorTest {
-//
-//  @Test
-//  fun `tank robot estimator interface`() {
-//
-//    val estimator1 = object : TankRobotVelocityEstimator {
-//      override fun estimateAbsoluteVelocity(): ScalarVector {
-//        return ScalarVector(0.0, -3.0)
-//      }
-//
-//      override fun getRightTranslationalWheelVelocity(): Double {
-//        return -3.0
-//      }
-//
-//      override fun getLeftTranslationalWheelVelocity(): Double {
-//        return -3.0
-//      }
-//    }
-//
-//    assertEquals(3.0, estimator1.avgTranslationalWheelSpeed)
-//    assertEquals(-3.0, estimator1.avgTranslationalWheelVelocity)
-//  }
-//}
+package com.github.ezauton.core.localization
+
+import com.github.ezauton.conversion.ConcreteVector
+import com.github.ezauton.conversion.LinearVelocity
+import com.github.ezauton.conversion.mps
+import com.github.ezauton.conversion.vec
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LocationEstimatorTest {
+
+  @Test
+  fun `tank robot estimator interface`() {
+
+    val estimator1 = object : TankRobotVelocityEstimator {
+      override fun estimateAbsoluteVelocity(): ConcreteVector<LinearVelocity> {
+        return vec(0.0, -3.0)
+      }
+
+      override val leftTranslationalWheelVelocity: LinearVelocity
+        get() = (-3.0).mps
+
+      override val rightTranslationalWheelVelocity: LinearVelocity
+        get() = (-3.0).mps
+    }
+
+    assertEquals(3.0.mps, estimator1.avgTranslationalWheelSpeed)
+    assertEquals((-3.0).mps, estimator1.avgTranslationalWheelVelocity)
+  }
+}
